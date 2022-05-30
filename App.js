e657c83d01380e667876dc00bc0c8deac7371700
zs <Zs1478963258963@iCloud.com>
2022-05-30 17:29:30 +0800

    加入学习笔记和readme，更新app.js内容

--- App.js
+++ App.js
@@ -1,111 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, FlatList} from 'react-native';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+  getMoviesFromApiAsync() {
+    fetch('https://reactnative.dev/movies.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({movies: responseJson.movies});
+        this.state.movies = responseJson.movies;
+        console.log(this.state.movies);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  componentDidMount() {
+    const movies = this.getMoviesFromApiAsync();
+  }
+  _onPressText() {
+    alert('点击成功');
+  }
+  renderItem = ({item}) => {
+    return (
+      <TouchableOpacity onPress={this._onPressText}>
+        <Text style={styles.button}>{item.title}</Text>
+      </TouchableOpacity>
+    );
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList data={this.state.movies} renderItem={this.renderItem} />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    marginTop: 50,
+    width: '100%',
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  button: {
+    // width: 100,
+    height: 100,
+    fontSize: 40,
+    textAlign: 'center',
+    lineHeight: 100,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  item1: {
+    width: 100,
+    height: 50,
+    backgroundColor: 'red',
   },
-  highlight: {
-    fontWeight: '700',
+  item2: {
+    width: 100,
+    height: 80,
+    backgroundColor: 'blue',
+  },
+  item3: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'yellow',
   },
 });
 
